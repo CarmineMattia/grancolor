a949b1864cd312ca7164804c4e0202b37a6c3c58
CarmineMattia <98carmine3@gmail.com>
2024-01-08 19:05:02 +0100

    update: 3.2

--- src/components/catalogs/Ironic.jsx
+++ src/components/catalogs/Ironic.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { Box, Tabs, Tab, Typography } from "@mui/material";
-import ironicBigImg from "../../assets/ironic-bigimg.png";
-// Import icons
-import WaterDropIcon from "@mui/icons-material/WaterDrop";
-import AirIcon from "@mui/icons-material/Air";
-import OpacityIcon from "@mui/icons-material/Opacity";
+import { Box, Tabs, Tab, Typography, Container } from "@mui/material";
+import BrushIcon from "@mui/icons-material/Brush"; // Icon for painting/coating
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome"; // Icon for aesthetic/dynamic effect
+import InnovationIcon from "@mui/icons-material/Lightbulb"; // Icon for innovation
+import ironicBigImg from "../../assets/ironic-img-bg.jpeg";
 
 function a11yProps(index) {
   return {
@@ -46,16 +45,17 @@ const Ironic = () => {
         variant="h4"
         align="center"
         gutterBottom
-        gutterTop
         style={{ margin: "2rem" }}
       >
         Ironic
       </Typography>
-      <img
-        src={ironicBigImg}
-        alt="Ironic Product"
-        style={{ width: "100%", height: "auto" }}
-      />
+      <Container maxWidth="md">
+        <img
+          src={ironicBigImg}
+          alt="Ironic Product"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </Container>
 
       <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
         <Tabs
@@ -64,45 +64,48 @@ const Ironic = () => {
           aria-label="basic tabs example"
         >
           <Tab label="Descrizione" {...a11yProps(0)} />
-          <Tab label="Caratteristiche" {...a11yProps(1)} />
         </Tabs>
       </Box>
 
       <TabPanel value={value} index={0}>
-        <p>
-          IRONic è un sistema a base d'acqua composto da una vernice di base
-          (IRONic Fondo) e una finitura ossidativa (IRONic Liquido
-          Antichizzante). L'applicazione di IRONic Liquido Antichizzante su
-          IRONic Fondo genera un effetto ossidativo identico a quello di una
-          superficie di ferro esposta alla corrosione naturale degli agenti
-          atmosferici. Le superfici trattate con IRONic sono dinamiche e
-          l'estetica può cambiare nel tempo. IRONic rappresenta un ponte tra
-          tradizione e innovazione, offrendo un sistema decorativo basato su
-          polimeri che permette di ottenere diversi effetti di ossidazione,
-          rispondendo alle esigenze specifiche dei clienti.
-        </p>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <Typography variant="h6" sx={{ display: "flex", alignItems: "center" }}>
-          <WaterDropIcon sx={{ mr: 1 }} /> Diluizione
-        </Typography>
-        <p>
-          Fondo Ironico: diluire con il 25-35% di acqua, sia per il primo che
-          per secondo strato.
-        </p>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <Typography
+            variant="h6"
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <BrushIcon sx={{ mr: 1 }} /> Sistema a Base d'Acqua
+          </Typography>
+          <p>
+            Ironic è un innovativo sistema decorativo a base d'acqua, che
+            include una vernice di base (Ironic Fondo) e una finitura ossidativa
+            (Ironic Liquido Antichizzante).
+          </p>
 
-        <Typography variant="h6" sx={{ display: "flex", alignItems: "center" }}>
-          <OpacityIcon sx={{ mr: 1 }} /> Resa
-        </Typography>
-        <p>
-          Fondo Ironic: 5-8 mq/l in due strati; ironico liquido antichizzante:
-          8-12 mq/l in due strati.
-        </p>
+          <Typography
+            variant="h6"
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <AutoAwesomeIcon sx={{ mr: 1 }} /> Effetto Estetico Dinamico
+          </Typography>
+          <p>
+            La finitura ossidativa crea un effetto unico, simile all'ossidazione
+            naturale del ferro, portando dinamicità e un'evoluzione estetica nel
+            tempo alle superfici.
+          </p>
 
-        <Typography variant="h6" sx={{ display: "flex", alignItems: "center" }}>
-          <AirIcon sx={{ mr: 1 }} /> Qualità dell'aria interna
-        </Typography>
-        <p>A+</p>
+          <Typography
+            variant="h6"
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <InnovationIcon sx={{ mr: 1 }} /> Innovazione e Tradizione
+          </Typography>
+          <p>
+            Ironic rappresenta la fusione perfetta tra tradizione artigianale e
+            innovazione tecnologica, offrendo diverse possibilità di
+            personalizzazione e adattandosi alle richieste specifiche di ogni
+            cliente.
+          </p>
+        </Box>
       </TabPanel>
     </Box>
   );
